c9d807a68993a17e72ffc4850b65edbe259acbce
Reinawan <admin@reinawan.com>
2024-01-29 21:29:26 +0700

    feat: connect with dotenv

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,28 +1,27 @@
-require('dotenv').config();
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 console.log(process.env.MONGO_URI);
-// const uri = process.env.MONGO_URI;
-const uri = "mongodb+srv://Reinawan07:Reinawan07@cluster0.hhidlho.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connect() {
+    try {
+        const database = client.db('Ch1');
+        const users = database.collection('users');
+        
+        const user = await users.find().toArray();
+        console.log(user);
+    } finally {
+       
+        await client.close();
+    }
 }
-run().catch(console.dir);
+
+module.exports = connect
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 
 const { typeDefs: userTypeDefs, resolvers: userResolvers } = require('./schema/user');
+const connect = require('./config/mongodb');
 
 const server = new ApolloServer({
   typeDefs: [userTypeDefs],
@@ -11,6 +13,7 @@ const server = new ApolloServer({
 startStandaloneServer(server, {
   listen: { port: 3000 },
 }).then(({ url }) => {
+  connect()
   console.log(`🚀  Server ready at: ${url}`);
 })
 
