2c192fcde8ff0e7aa832ec8d8eca4086d8fb8bb4
Reinawan <admin@reinawan.com>
2024-02-02 15:44:58 +0700

    style: TabBarIcon

--- app/App.js
+++ app/App.js
@@ -8,6 +8,8 @@ import Profile from './screen/Profile';
 import Login from './screen/Login';
 import Register from './screen/Register';
 import DetailPost from './screen/DetailPost';
+import { AntDesign, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import { Button } from 'react-native';
 
 
 const Stack = createNativeStackNavigator();
@@ -17,11 +19,38 @@ const Tab = createBottomTabNavigator();
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
-      <Tab.Screen name="CreatePost" component={CreatePost} options={{ headerShown: false }} />
-      <Tab.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="CreatePost"
+        component={CreatePost}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="pencil" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
 
 
@@ -31,7 +60,14 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={TabNavigator} options={{ title: 'Facebook' }} />
+        <Stack.Screen name="Home" component={TabNavigator} options={{ title: 'Facebook',
+          headerRight: () => (
+            <Button
+              onPress={() => alert('Are you sure?')}
+              title="Logout"
+              color="#1877f2"
+            />
+          ), }} />
         <Stack.Screen name="DetailPost" component={DetailPost} />
       </Stack.Navigator>
     </NavigationContainer>
